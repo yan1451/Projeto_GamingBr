81bfe53ef6b0271851e5a7ddb2db505a114da49e
Yan Brasileiro <yan.brasileiro@gmail.com>
2023-05-29 12:01:17 -0300

    update: rota verificar horarios restaurantes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Index from './paginas';
+import VerficiarFuncionamento from './componentes/VerificarFuncionamento';
 
 
 function App() {
   return (
     <Routes> 
       <Route path='/' element={ <Index /> }/>
+      <Route path='/verificar' element={ <VerficiarFuncionamento /> }/>
     </Routes>
   );
 }
