8d95cd02c45a9cb60a72e22725efdd4e6e56518a
Yan Brasileiro <yan.brasileiro@gmail.com>
2023-05-29 11:56:03 -0300

    update: formularios para cadastro dos horarios

--- frontend/src/componentes/cadastroHorarioFuncionamento.jsx
+++ frontend/src/componentes/cadastroHorarioFuncionamento.jsx
@@ -1,34 +1,79 @@
 import React from "react";
+import '../styles/componentes/cadastroHorarioFuncionamento.css'
 
-const CadastroFuncionamento = () => {
-    return(
-        <section className="cadastro-restaurantes">
-        <form>
-          <h1>Definir horario de Funcionamento</h1>
-          <label htmlFor="name-input">
+const CadastroFuncionamento = ({ data, handleChange }) => {
+  return (
+    <section className="cadastro-horariosFuncionamento">
+      <form>
+        <h1>Definir horario de Funcionamento</h1>
+        <div>
+          <label htmlFor="input-segundaFeira">
             <input
-              id="name-input"
+              id="input-segundaFeira"
               type="text"
-              placeholder="Nome"
+              value={data.segunda}
+              placeholder="Segunda-feira"
+              onChange={(e) => handleChange("segunda", e.target.value)}
             />
           </label>
-          <label htmlFor="documento-input">
+          <label htmlFor="input-tercaFeira">
             <input
-              id="documento-input"
+              id="input-tercaFeira"
               type="text"
-              placeholder="Documento"
+              value={data.terça}
+              placeholder="Terça-feira"
+              onChange={(e) => handleChange("terça", e.target.value)}
             />
           </label>
-          <label htmlFor="tipo-input">
+          <label htmlFor="input-quartaFeira">
             <input
-              id="tipo-input"
+              id="input-quartaFeira"
               type="text"
-              placeholder="Tipo"
+              value={data.quarta}
+              placeholder="Quarta-feira"
+              onChange={(e) => handleChange("quarta", e.target.value)}
             />
           </label>
-        </form>
-      </section>
-    );
+          <label htmlFor="input-quintaFeira">
+            <input
+              id="input-quintaFeira"
+              type="text"
+              value={data.quinta}
+              placeholder="Quinta-feira"
+              onChange={(e) => handleChange("quinta", e.target.value)}
+            />
+          </label>
+          <label htmlFor="input-sextaFeira">
+            <input
+              id="input-sextaFeira"
+              type="text"
+              value={data.sexta}
+              placeholder="Sexta-feira"
+              onChange={(e) => handleChange("sexta", e.target.value)}
+            />
+          </label>
+          <label htmlFor="input-sabado">
+            <input
+              id="input-sabado"
+              type="text"
+              value={data.sábado}
+              placeholder="Sabado"
+              onChange={(e) => handleChange("sábado", e.target.value)}
+            />
+          </label>
+          <label htmlFor="input-domingo">
+            <input
+              id="input-domingo"
+              type="text"
+              value={data.domingo}
+              placeholder="Domingo"
+              onChange={(e) => handleChange("domingo", e.target.value)}
+            />
+          </label>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default CadastroFuncionamento;
