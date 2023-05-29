7d3bb538e1a12c2b506cc7894b3f49bfa42b8e72
Yan Brasileiro <yan.brasileiro@gmail.com>
2023-05-29 11:58:27 -0300

    feat: pagina horarios de funcionamento

---
+++ frontend/src/paginas/horario_funcionamento.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VerficiarFuncionamento from '../componentes/BtnVerificarFuncionamento';
+
+const HorarioFuncionamento = () => {
+  return (
+    <>
+   <VerficiarFuncionamento />
+   </>
+  )
+};
+
+export default HorarioFuncionamento;
